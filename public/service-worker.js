11171febf58336240fde8ebefa4fe759cb805e5a
jayanth5595-droid <jayanth5595@gmail.com>
2026-09-03 14:21:04 +0530

    Add files via upload

---
+++ public/service-worker.js
@@ -0,0 +1,51 @@
+const CACHE_NAME = 'dream-home-v3';
+
+self.addEventListener('install', event => {
+  self.skipWaiting();
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache =>
+      cache.addAll([
+        './',
+        './index.html',
+        './manifest.webmanifest',
+        './icons/icon-192.png',
+        './icons/icon-512.png',
+        './icons/icon-512-maskable.png'
+      ])
+    )
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys
+          .filter(key => key !== CACHE_NAME)
+          .map(key => caches.delete(key))
+      )
+    ).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', event => {
+  const request = event.request;
+  const url = new URL(request.url);
+
+  if (request.method !== 'GET') return;
+  if (url.hostname.includes('supabase.co')) return;
+
+  event.respondWith(
+    fetch(request)
+      .then(response => {
+        if (response.ok) {
+          const copy = response.clone();
+          caches.open(CACHE_NAME).then(cache => cache.put(request, copy));
+        }
+        return response;
+      })
+      .catch(() =>
+        caches.match(request).then(cached => cached || caches.match('./'))
+      )
+  );
+});
